3386a5f128d19ebab6a01cc2be576ca650f0950d
DSwan <dianaswan0@gmail.com>
2024-10-06 20:08:46 +0000

    and JS event listeners

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+}
+
+let citySearchFormElement = document.querySelector("#city-Search-Form");
+citySearchFormElement.addEventListener("submit", handleSearchSubmit);
